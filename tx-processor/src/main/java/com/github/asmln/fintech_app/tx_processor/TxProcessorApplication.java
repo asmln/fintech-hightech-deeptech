66fc54501f086a4c83f1a51a95f7eeb2a6205710
sag <a.smln@yandex.ru>
2026-09-03 16:33:37 +0300

    Добавил вывод информации о миграции.

--- tx-processor/src/main/java/com/github/asmln/fintech_app/tx_processor/TxProcessorApplication.java
+++ tx-processor/src/main/java/com/github/asmln/fintech_app/tx_processor/TxProcessorApplication.java
@@ -1,11 +1,26 @@
 package com.github.asmln.fintech_app.tx_processor;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TxProcessorApplication {
   static void main(String[] args) {
     SpringApplication.run(TxProcessorApplication.class, args);
+  }
+  @Bean
+  public CommandLineRunner checkMigrations(Flyway flyway) {
+    return args -> {
+      // Информация о миграциях
+      var info = flyway.info();
+      System.out.println("=== FLYWAY MIGRATIONS ===");
+      System.out.println("Current version: " + info.current().getVersion());
+      System.out.println("Pending migrations: " + info.pending().length);
+      System.out.println("Applied migrations: " + info.applied().length);
+      System.out.println("=========================");
+    };
   }
 }
