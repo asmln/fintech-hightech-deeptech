b583f90c7e35e0bbb91ed7601b0aa4ee06955001
sag <a.smln@yandex.ru>
2026-09-04 17:48:29 +0300

    Добавил настройки kafka, зависимости kafka и jackson.

--- tx-processor/build.gradle.kts
+++ tx-processor/build.gradle.kts
@@ -12,11 +12,16 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-kafka")
+    implementation("org.springframework.boot:spring-boot-starter-json")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     implementation(libs.postgresql)
 
     testImplementation(platform(libs.junit.bom))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
 
     //--- Обслуживание кода
     errorprone(libs.errorprone.core)
