37c3a336f14a63bf56d7e67f8a4bd2254f2254d6
sag <a.smln@yandex.ru>
2026-09-18 00:05:59 +0300

    Убрал проверку миграции через Bean.

--- tx-processor/src/main/java/com/github/asmln/fintech_app/tx_processor/TxProcessorApplication.java
+++ tx-processor/src/main/java/com/github/asmln/fintech_app/tx_processor/TxProcessorApplication.java
@@ -1,10 +1,7 @@
 package com.github.asmln.fintech_app.tx_processor;
 
-import org.flywaydb.core.Flyway;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -12,18 +9,5 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class TxProcessorApplication {
     static void main(String[] args) {
         SpringApplication.run(TxProcessorApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner checkMigrations(Flyway flyway) {
-        return args -> {
-            // Информация о миграциях
-            var info = flyway.info();
-            System.out.println("=== FLYWAY MIGRATIONS ===");
-            System.out.println("Current version: " + info.current().getVersion());
-            System.out.println("Pending migrations: " + info.pending().length);
-            System.out.println("Applied migrations: " + info.applied().length);
-            System.out.println("=========================");
-        };
     }
 }
